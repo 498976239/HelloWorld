e5ec2303ead40fecfaaba98a93c6c0c9b701fa56
yangxuesong <498976239@qq.com>
2017-08-08 16:26:11 +0800

    private int i ;     private int w ;

--- app/src/main/java/com/ss/www/helloworld/MainActivity.java
+++ app/src/main/java/com/ss/www/helloworld/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int i ;
+    private int w ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
